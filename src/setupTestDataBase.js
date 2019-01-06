477a58fcf609db56f9175b1d960ec0f8ac04fa94
patrick <patmagauran.j@gmail.com>
2019-01-06 11:24:42 -0500

    Deployment tests work - correct mysql port

--- src/setupTestDataBase.js
+++ src/setupTestDataBase.js
@@ -1,25 +1,18 @@
 /* eslint-disable no-console */
 const mysql = require('mysql2/promise');
 const config = require('config');
-const {Client} = require('pg');
 //...
 async function setup() {
+  var connection;
+  var database;
   if (process.env.NODE_ENV === 'test') {
     let connectionString = process.env.DATABASE_URL;
-
-    const client = new Client({
-      connectionString: connectionString,
-    });
-    client.connect();
-
-    client.query('SELECT NOW()', (err, res) => {
-      console.log(err, res);
-      client.end();
-    });
+    database = process.env.MYSQL_DATABASE;
+    connection = await mysql.createConnection(connectionString);
   } else {
+
     const connectionDetails = config.get('mysql');
-
-
+    database = connectionDetails.database;
     /*  const sequelize = new Sequelize(connectionDetails.database, connectionDetails.username, connectionDetails.password, {
     dialect: 'mysql',
     host: connectionDetails.host,
@@ -31,19 +24,20 @@ async function setup() {
     },
     dialectOptions: connectionDetails.dialectOptions,
   });*/
-    var connection = await mysql.createConnection({
+    connection = await mysql.createConnection({
       host: connectionDetails.host,
       user: connectionDetails.username,
       password: connectionDetails.password
 
     });
 
-    connection.connect();
-    await connection.execute('DROP DATABASE IF EXISTS ' + connectionDetails.database);
-    await connection.execute('CREATE DATABASE IF NOT EXISTS ' + connectionDetails.database);
 
-    connection.end();
   }
+  connection.connect();
+  await connection.execute('DROP DATABASE IF EXISTS ' + database);
+  await connection.execute('CREATE DATABASE IF NOT EXISTS ' + database);
+
+  connection.end();
 
 }
 
