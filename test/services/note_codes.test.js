66140bf9ac200d54031de3ee247e3e118c67ba7c
Patrick J. Magauran <patmagauran.j@gmail.com>
2019-05-14 09:13:12 -0400

    Fix service registration tests for note_codes and payment_methods

--- test/services/note_codes.test.js
+++ test/services/note_codes.test.js
@@ -3,7 +3,7 @@ const app = require('../../src/app');
 
 describe('\'note_codes\' service', () => {
   it('registered the service', () => {
-    const service = app.service('note-codes');
+    const service = app.service('note_codes');
 
     assert.ok(service, 'Registered the service');
   });
