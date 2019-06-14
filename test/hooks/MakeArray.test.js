0d8ed8978be07377d616b49a716b7464afe0e74f
patrick <patmagauran.j@gmail.com>
2019-06-14 19:38:20 -0400

    Make Array Hook Test Implemented.

--- test/hooks/MakeArray.test.js
+++ test/hooks/MakeArray.test.js
@@ -1,210 +1,78 @@
-const rewire = require('rewire');
-const {Forbidden} = require('@feathersjs/errors');
 
-const assert = require('assert');
 const should = require('should');
-const feathers = require('@feathersjs/feathers');
-const checkPermissions = rewire('../../src/hooks/check-permissions');
-const {createYears} = require('../../src/databaseCreators/years');
-const {createUsers, createAdmin} = require('../../src/databaseCreators/users');
-const {createUsersManagement} = require('../../src/databaseCreators/user_managers');
-const app = require('../../src/app');
-const {cleanup} = require('../../src/databaseCreators/cleanup');
+const MakeArray = require('../../src/hooks/makeArray');
 
-describe('\'checkPermissions\' hook', () => {
-  step('Creating Years', function(done)  {
-    this.timeout(10000);
 
-    createYears(app).then((res, err) => {
-      done(err);
-    });
-  });
-  step('Creating Users', function(done)  {
-    this.timeout(10000);
+describe('\'Make-Array\' hook', () => {
 
-    createUsers(app).then((res, err) => {
-      done(err);
-    });
-  });
-  step('Creating Admin Users', function(done)  {
-    this.timeout(10000);
 
-    createAdmin(app).then((res, err) => {
-      done(err);
-    });
-  });
-  step('Creating userMangagers', function(done)  {
-    this.timeout(10000);
 
-    createUsersManagement(app).then((res, err) => {
-      done(err);
-    });
-  });
-
-  /*it('Get Year for Enabled', () => {
-    let fakeContext = {params: {
-        user: {
-          enabledYear: '2014'
-        }
-      }};
-
-    let returnYear = filterManagedUsers.__get__('getYear')(fakeContext);
-
-    returnYear.should.equal('2014');
-  });
-  it('Get Year for Query ID', () => {
+  it('Test Before', function () {
     let fakeContext = {
-      params: { query: {
-          year_id: '2014'
-        }
-        , user: {id: 0}
-      }};
-
-    let returnYear = filterManagedUsers.__get__('getYear')(fakeContext);
-
-    returnYear.should.equal('2014');
-  });
-  it('Get Year for Query', () => {
-    let fakeContext = {
-      params: {
-        query: {
-          year: '2014'
-        },
-        user: {id: 0}
-      }
-    };
-
-
-    let returnYear = filterManagedUsers.__get__('getYear')(fakeContext);
-
-    returnYear.should.equal('2014');
-  });
-  */
-  it('Test Same Role Level', function (done) {
-    this.timeout(10000);
-
-    let fakeContext = {
-      app: app,
+      type: 'before',
       method: 'update',
-      data: {
-        user_id: 3,
-      },
-
-      params: {
-        provider: 'rest',
-        query: {
-        },
-        payload: {
-          userId: 2,
-        },
-        user: {
-          id: 2,
-          enabledYear: 20
-        },
-      },
-      path: 'dummy'
+      data: {key: 'element'}
     };
+    let returnData = MakeArray()(fakeContext);
 
-    checkPermissions(['ROLE_USER'])(fakeContext).then((filter) => {
-      filter.should.deepEqual(fakeContext);
-      done();
-    })
-      .catch((err) => {
-        done(err);
-      });
+    returnData.data.should.deepEqual([{key: 'element'}]);
   });
-  it('Test Sub Role Level', function (done) {
-    this.timeout(10000);
-
+  it('Test Before 2 items', function () {
     let fakeContext = {
-      app: app,
+      type: 'before',
       method: 'update',
-      data: {
-        user_id: 3,
-      },
-
-      params: {
-        provider: 'rest',
-        query: {
-        },
-        payload: {
-          userId: 5,
-        },
-        user: {
-          id: 5,
-          enabledYear: 20
-        },
-      },
-      path: 'dummy'
+      data: [{key: 'element'}, {key: 'element2'}]
     };
+    let returnData = MakeArray()(fakeContext);
 
-    checkPermissions(['ROLE_USER'])(fakeContext).then((filter) => {
-      filter.should.deepEqual(fakeContext);
-      done();
-    })
-      .catch((err) => {
-        done(err);
-      });
+    returnData.data.should.deepEqual( [{key: 'element'}, {key: 'element2'}]);
   });
-  it('Test Invalid Role(User accessing admin) - Checks related roles', function () {
-    this.timeout(10000);
-
+  it('Test After Update', function () {
     let fakeContext = {
-      app: app,
+      type: 'after',
       method: 'update',
-      data: {
-        user_id: 3,
-      },
+      data: {key: 'element'},
+      result: {key: 'element'}
 
-      params: {
-        provider: 'rest',
-        query: {
-        },
-        payload: {
-          userId: 2,
-        },
-        user: {
-          id: 2,
-          enabledYear: 20
-        },
-      },
-      path: 'dummy'
     };
+    let returnData = MakeArray()(fakeContext);
 
-    return checkPermissions(['ROLE_ADMIN'])(fakeContext).should.be.rejectedWith(Forbidden);
+    returnData.data.should.deepEqual([{key: 'element'}]);
+    returnData.result.should.deepEqual([{key: 'element'}]);
   });
-  it('Test Invalid Role(User accessing test) - Checks unrelated roles', function () {
-    this.timeout(10000);
-
+  it('Test After Create', function () {
     let fakeContext = {
-      app: app,
-      method: 'update',
-      data: {
-        user_id: 3,
-      },
-
-      params: {
-        provider: 'rest',
-        query: {
-        },
-        payload: {
-          userId: 2,
-        },
-        user: {
-          id: 2,
-          enabledYear: 20
-        },
-      },
-      path: 'dummy'
+      type: 'after',
+      method: 'create',
+      data:{key: 'element'},
+      result: {key: 'element'}
     };
+    let returnData = MakeArray()(fakeContext);
 
-    return checkPermissions(['ROLE_TEST'])(fakeContext).should.be.rejectedWith(Forbidden);
+    returnData.result.should.deepEqual([{key: 'element'}]);
   });
-  after('Cleanup', function(done)  {
-    this.timeout(10000);
+  it('Test After Update 2 items', function () {
+    let fakeContext = {
+      type: 'after',
+      method: 'update',
+      data: [{key: 'element'}, {key: 'element2'}],
+      result: [{key: 'element'}, {key: 'element2'}]
+    };
+    let returnData = MakeArray()(fakeContext);
 
-    cleanup(app).then((res, err) => {
-      done(err);
-    });
+    returnData.data.should.deepEqual( [{key: 'element'}, {key: 'element2'}]);
+    returnData.result.should.deepEqual( [{key: 'element'}, {key: 'element2'}]);
   });
+  it('Test After Create 2 items', function () {
+    let fakeContext = {
+      type: 'after',
+      method: 'create',
+      data: [{key: 'element'}, {key: 'element2'}],
+      result: [{key: 'element'}, {key: 'element2'}]
+    };
+    let returnData = MakeArray()(fakeContext);
+
+    returnData.result.should.deepEqual( [{key: 'element'}, {key: 'element2'}]);
+  });
+
 });
